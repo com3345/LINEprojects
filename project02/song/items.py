067a11bc5850829e3d0e92b5d02b1d1a775fcbe0
com3345 <deardoublelee@gmail.com>
2016-06-20 10:01:53 +0900

    comments added

--- project02/song/items.py
+++ project02/song/items.py
@@ -1,13 +1,10 @@
 # -*- coding: utf-8 -*-
 
-# Define here the models for your scraped items
-#
-# See documentation in:
-# http://doc.scrapy.org/en/latest/topics/items.html
 
 import scrapy
 
 
+# Built-in implementation of ORM
 class SongItem(scrapy.Item):
     name = scrapy.Field()
     singer = scrapy.Field()
--- project02/song/spiders/kasitime_spider.py
+++ project02/song/spiders/kasitime_spider.py
@@ -1,3 +1,18 @@
+# -*- coding: utf-8 -*-
+
+'''
+A spider for crawling meta info of songs whose name is in
+    the mecab-ipadic-neologd dictionary
+    (https://github.com/neologd/mecab-ipadic-neologd)
+from 歌詞タイム sites
+    (http://www.kasi-time.com)
+then save those info into csv file as following format:
+    name, singer, lyricist, composer, arranger, lyrics
+
+When two songs have the same name, remove one whose google
+search results is lesser.
+'''
+
 import scrapy
 import MeCab
 from song.items import SongItem
@@ -7,6 +22,8 @@ import logging
 class KasiTimeSpider(scrapy.Spider):
 
     name = "kasitime"
+
+    # default numbers for sites to be crawled
     MAX_PAGES = 79082
 
     def __init__(self, n_pages=MAX_PAGES, *args, **kwargs):
@@ -14,6 +31,8 @@ class KasiTimeSpider(scrapy.Spider):
         self.start_urls = (
             "http://www.kasi-time.com/item-{0}.html".format(str(i))
             for i in range(1, int(n_pages) + 1))
+
+        # set xpath and regex for each item fields
         self._set_xpath_and_regex()
         self._mt = MeCab.Tagger()
 
@@ -32,9 +51,12 @@ class KasiTimeSpider(scrapy.Spider):
     def parse(self, response):
         word = response.xpath(
             self._r['name'][0]).re(self._r['name'][1])[0].strip()
+
+        # if song name is not in neologd, skip this response
         if self._not_in_dict(word):
             logging.info('{0} is not in neologd'.format(word))
             return
+
         item = SongItem()
         for key in self._r:
             values = response.xpath(self._r[key][0]).re(self._r[key][1])
